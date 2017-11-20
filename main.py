e180932dc0dedd4aebf8e2a19d128159aa612d71
Salvador Guzman <guzmansalv@gmail.com>
2017-11-20 04:44:54 -0800

    refactored to make code uniform

--- main.py
+++ main.py
@@ -6,17 +6,20 @@ r2 = requests.get('https://www.uber.com/')
 r3 = requests.get('https://partners.uber.com/login')
 r4 = requests.get('https://auth.uber.com/login/?next_url=https%3A%2F%2Fpartners.uber.com')
 
-head1 = {'Content-Type': 'application/json', 'x-csrf-token': r4.headers['x-csrf-token']}
-r5 = requests.post('https://auth.uber.com/login/handleanswer',
-         headers=head1,
-         data='{"answer":{"type":"VERIFY_INPUT_EMAIL","userIdentifier":{"email":"' + sys.argv[1] + '"}},"init":true}',
-         cookies=r4.cookies)
 
-head2 = {'Content-Type': 'application/json', 'x-csrf-token': r5.headers['x-csrf-token']}
-r6 = requests.post('https://auth.uber.com/login/handleanswer',
-         headers=head2,
-         data='{"answer":{"type":"VERIFY_PASSWORD","password":"' + sys.argv[2] + '"},"rememberMe":true}',
-         cookies=r5.cookies)
+def handleanswer(request, resp, data):
+    return request.post('https://auth.uber.com/login/handleanswer',
+                        headers={'Content-Type': 'application/json', 'x-csrf-token': resp.headers['x-csrf-token']},
+                        data=data,
+                        cookies=resp.cookies)
+
+
+r5 = handleanswer(requests, r4,
+                  '{"answer":{"type":"VERIFY_INPUT_EMAIL","userIdentifier":{"email":"'
+                  + sys.argv[1] + '"}},"init":true}')
+
+r6 = handleanswer(requests, r5,
+                  '{"answer":{"type":"VERIFY_PASSWORD","password":"' + sys.argv[2] + '"},"rememberMe":true}')
 
 print(r6.status_code)
 print(r6.text)
